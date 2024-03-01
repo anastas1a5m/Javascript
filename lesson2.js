86f6252ec0595d1cf69bb5ff678fc56b5dad607e
anastasia <anastasia.miliutina@gmail.com>
2024-03-01 19:57:24 +0200

    Lesson 2

---
+++ lesson2.js
@@ -0,0 +1,96 @@
+
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+
+//console.log(Boolean(1 > 0)); // true
+//console.log(Boolean(0 > 0)); // true
+//console.log(Boolean(-3 > 0)); // false
+
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+
+//const test = confirm("true");
+// console.log("test");
+// console.log("qwerty");
+// console.log(true);
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+//  if (10 > 1) {
+//      console.log(1-5); //-4
+//  } else if  (10 < 1) {
+//      console.log(1+5);
+// } else {
+//      console.log("no");
+//  }
+//
+// if (10 > 10) {
+//     console.log(10-5);
+// } else if  (10 < 10) {
+//     console.log(10+5);
+// } else {
+//     console.log("no"); //no
+// }
+//
+// if (10 > 13) {
+//     console.log(13-5);
+// } else if  (10 < 13) {
+//     console.log(13+5); //18
+// } else {
+//     console.log("no");
+// }
+
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// const min = prompt("Введіть число місяця");
+//  if (min === "1"){
+//      console.log("January");
+//  } else if  (min === "2"){
+//      console.log("February");
+//  } else if (min === "3"){
+//      console.log("March");
+//  } else if (min === "4"){
+//      console.log("April");
+//  } else if (min === "5"){
+//      console.log("May");
+//  } else if (min === "6"){
+//      console.log("June");
+//  } else if (min === "7"){
+//      console.log("July");
+//  } else if (min === "8"){
+//      console.log("August");
+//  } else if (min === "9"){
+//      console.log("September");
+//  } else if (min === "10"){
+//      console.log("October");
+//  } else if (min === "11"){
+//      console.log("November");
+//  } else if (min === "12"){
+//      console.log("December");
+//  }
+
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// const num = "666";
+// let first = Math.floor(num / 100);
+// console.log(first);
+// let second = Math.floor((num - first * 100) / 10);
+// console.log(second);
+// let third = num - second * 10 - first * 100;
+// console.log(third);
+// console.log(first+second+third); //18
+
+
+
+
+
+
+
+
